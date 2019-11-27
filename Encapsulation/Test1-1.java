b609bdb88489b7bc80fcd3ca98895e90c74e7aad
Manuela Dorado Novoa <manuedorado04516@gmail.com>
2019-11-27 15:27:47 -0800

    Add files via upload

---
+++ Encapsulation/Test1-1.java
@@ -0,0 +1,30 @@
+
+public class Test1 {
+	String name;
+	private int age;
+	protected int ID;
+
+	public void setName(String n) {
+		this.name=n;
+	}
+	
+	public void setAge(int a) {
+		this.age=a;
+	}
+	
+	public void setID(int i) {
+		this.ID=i;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public int getId() {
+		return ID;
+	}
+}
---
+++ Encapsulation/Test2-1.java
@@ -0,0 +1,17 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		Test1 mt = new Test1();
+		
+		mt.setName("Manuela Dorado Novoa");
+		mt.setAge(24);
+		mt.setID(123456789);
+		
+		System.out.println(mt.getName());
+		System.out.println(mt.getAge());
+		System.out.println(mt.getId());
+	}
+
+}
